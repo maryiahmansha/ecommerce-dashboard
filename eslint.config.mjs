af7b92e5c0714ff6f3aa2ca9a56ca86587b03116
Maryiah <marymansha15@gmail.com>
2025-09-03 13:39:50 +0100

    finalise linting/husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import prettierPlugin from "eslint-plugin-prettier";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,30 +11,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
-      prettier: prettierPlugin
+      prettier: prettierPlugin,
     },
     rules: {
       // Ensure Prettier show up as ESLint errors
-      "prettier/prettier": "error",
+      'prettier/prettier': 'error',
 
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-      "no-unused-vars": "off", // handled by TS
-      "@typescript-eslint/no-unused-vars": ["warn"],
-      "react/react-in-jsx-scope": "off" // not needed in Next.js
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-unused-vars': 'off', // handled by TS
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'react/react-in-jsx-scope': 'off', // not needed in Next.js
     },
   },
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
 ];
 
